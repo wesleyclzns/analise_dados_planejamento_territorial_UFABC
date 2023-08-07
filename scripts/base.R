04427ce8613a5ca90da843e618e254bb84999ca4
wesleyclzns <calazanscafe@outlook.com>
2023-08-07 20:47:30 -0300

    função de geração de graficos

--- scripts/base.R
+++ scripts/base.R
@@ -45,3 +45,33 @@ cor.test(x = dadox,
 #p-value = 2.2e-16
 # Aceitamos a hipotese nula
 #Nãoe existi alguma correlação entre a renda per capita e o tempo medio de viagens em transporte coletivo
+
+#============================================================================
+#Função
+
+Dispercao = function (df, dadox , dadoy, labelDadoX, labelDadoy) {
+  
+  #PLOT
+  plot(x = dadox,
+       y = dadoy,
+       xlab = labelDadoX,
+       ylab = labelDadoy)
+  
+  fd <- na.omit(fd) # Remove observações com valores NA
+  
+  # Remove observações com valores ausentes ou infinitos da variável y
+  data_clean <- na.omit(df)
+  
+  # Ajustar o modelo de regressão linear com os dados limpos
+  lm_model <- lm(y ~ x, data = data_clean)
+  
+  #GGPLOT
+  ggplot(data = fd, aes(x = dadox, y = dadoy)) +   
+    geom_point() +   geom_smooth(data = lm(formula = dadoy ~ dadox, data = df), 
+                                 method = "lm", col = "red", se = FALSE) + theme_bw() +   
+    xlab(labelDadoX) +   
+    ylab(labelDadoy)
+  
+}
+
+#============================================================================

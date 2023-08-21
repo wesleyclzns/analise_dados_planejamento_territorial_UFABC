8b7cfec26a651a6073cb64d6324a206bbf24f14d
wesleyclzns <calazanscafe@outlook.com>
2023-08-21 18:52:38 -0300

    função de dispersão esta funcionando

--- scripts/funcao_dispersao.R
+++ scripts/funcao_dispersao.R
@@ -1,21 +1,3 @@
-# D:/CM/ADPT (setwd)
-
-#Instalando pacotes
-install.packages("tidyverse")
-install.packages("performance")
-install.packages("broom")
-
-#Importando
-library(tidyverse)
-library(performance)
-library(broom)
-
-agua_rede1 <- read.csv2("https://raw.githubusercontent.com/luisfelipebr/mti/master/dados/agua_rede1.csv", encoding="UTF-8")
-
-names(agua_rede1)
-modelo1 <- lm(formula = CONSUMO1 ~ RENDAPITA, data = agua_rede1, na.action = na.exclude)
-summary(modelo1)
-
 pesOD <- read.csv("D:/CM/ADPT/dados/pesOD_taxas.csv")
 
 head(pesOD)
